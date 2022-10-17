b3cc47b959ee1f0457240cbb10a3bdcd5ae3216d
GIORGIO AMATI <gamati01@login02.m100.cineca.it>
2022-10-17 17:20:27 +0200

    fix

--- UTIL/RunMe_thread.sh
+++ UTIL/RunMe_thread.sh
@@ -1,7 +1,8 @@
 #
 for th in 64 32 16 8 4 2 1; do
 	export ACC_NUM_CORES=$th
-	echo "Running threads = "$ACC_NUM_CORES
-	./bgk_thermal > out.$ACC_NUM_CORES.log
-	md5sum Output*50000.vtk > check.$ACC_NUM_CORES.log
+	export OMP_NUM_THREADS=$th
+	echo "Running threads = "$ACC_NUM_CORES, $OMP_NUM_THREADS
+	./bgk_thermal > out.$th.log
+	md5sum Output*50000.vtk > check.$th.log
 done
