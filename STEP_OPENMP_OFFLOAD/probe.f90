68cfe0eedd6d87c8a79371999354ad54984f1084
GIORGIO AMATI <g.amati@cineca.it>
2022-10-17 16:52:50 +0200

    fix bug on probe.f90

--- STEP_OPENMP_OFFLOAD/probe.f90
+++ STEP_OPENMP_OFFLOAD/probe.f90
@@ -4,13 +4,16 @@
 
   use shared
 
-     write (666,1001) it,             u(Nx/2,Ny/2),   v(Nz/2,Ny/2), & 
-                    rho(Nx/2,Ny/2), T(Nx/2,Ny/2), phi(Nz/2,Ny/2)
+     write (666,1001) it,             &
+                      u(Nx/2,Ny/2),   &
+                      v(Nx/2,Ny/2),   & 
+                      rho(Nx/2,Ny/2), & 
+                      T(Nx/2,Ny/2),   & 
+                      phi(Nx/2,Ny/2)
 
 ! formats...
 1001    format(i8,5(e14.6,1x))
 
- 
 #ifdef DEBUG
      write(6,*) "DEBUG: Completed subroutine probe"
 #endif
