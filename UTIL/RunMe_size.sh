4bd8dab663fd27c99c451a178ca0b30c610c7c70
Giorgio Amati <g.amati@cineca.it>
2022-10-19 15:38:26 +0200

    improved script

--- UTIL/RunMe_size.sh
+++ UTIL/RunMe_size.sh
@@ -3,4 +3,5 @@ for inp in 0 1 2 3 ; do
 	echo "Running size = input.dat."$inp
 	cp input.dat.$inp input.dat
 	./bgk_thermal > out.size.$inp.log
+	grep Mlups  out.size.$inp.log
 done
--- UTIL/RunMe_thread.sh
+++ UTIL/RunMe_thread.sh
@@ -5,4 +5,5 @@ for th in 64 32 16 8 4 2 1; do
 	echo "Running threads = "$ACC_NUM_CORES, $OMP_NUM_THREADS
 	./bgk_thermal > out.$th.log
 	md5sum Output*50000.vtk > check.$th.log
+	grep Mlups  out.$th.log
 done
